76a21cda2b7e4751552bad78610c85530f541263
Joseph Scavone <josephscavone@gmail.com>
2012-05-25 01:04:21 -0500

    corrected the table name

--- kudos.php
+++ kudos.php
@@ -4,7 +4,8 @@ require('./wp-blog-header.php');
 $post_id = $_POST['article'];
 $cooking = $_POST['cooking'];
 
-$table = $wpdb->prefix . 'post_meta';
+$table = $wpdb->prefix . 'postmeta';
+
 $sql = "SELECT meta_value FROM $table WHERE post_id = $post_id AND meta_key = '_wp-svbtle-kudos'";
 
 $kudos = $wpdb->get_var( $wpdb->prepare( $sql ) );
